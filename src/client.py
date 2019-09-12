0b332c52e49fac54a1fe703e10aef72b2f9021e6
Ivan Chavarria <ivanjavier2000@gmail.com>
2019-09-12 08:15:05 -0600

    Se arregla error de IP. Se logra comunicar con todos los grupos. Realizado por todo el grupo. Close #1

--- src/client.py
+++ src/client.py
@@ -3,31 +3,28 @@ import struct
 import time
 import random
 
-UDP_IP = "10.1.138.73"
-UDP_PORT = 65432
+UDP_IP = "10.1.137.79"
+MINE = "10.1.137.17"
+UDP_PORT = 10002
+
 SHOCK_ID = 0x04
 MOV_ID = 0x01
 KEEP_ALIVE = 0x0
 TEAM_ID = 0x03
 BOOL_TYPE = 0x01
 
-
-date = time.time()
-randomId= random.randint(0,65536)
+date = int( time.time() )
+randomId= random.randint(0,255)
 sensorId = 000
 code = KEEP_ALIVE
 data = True
 
 
-var = struct.pack('iiii?',randomId,date,sensorId,code,data) 
-
-#print "UDP target IP:", UDP_IP
-#print "UDP target port:", UDP_PORT
-#print "message:", MESSAGE
+var = struct.pack('BiiB?',randomId,date,sensorId,code,data) 
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind(("0.0.0.0", UDP_PORT))
+sock.bind((MINE, UDP_PORT))
 
 sock.sendto(var, (UDP_IP, UDP_PORT))
 
@@ -35,8 +32,8 @@ data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
 ACK = struct.unpack('ii', data)
 
 if randomId == ACK[0] :
-	print "Mensaje enviado y recibido correctamente"
+	print ("Mensaje enviado y recibido correctamente")
 else :
-	print "Mensaje enviado y recibido incorrectamente"
+	print ("Mensaje enviado y recibido incorrectamente")
 
-print "received message:", ACK[0], ACK[1]
+print ( "received message:", ACK[0], ACK[1] )
--- src/server.py
+++ src/server.py
@@ -3,20 +3,21 @@ import struct
 import time
 
 
+MINE = "10.1.137.17"
 UDP_IP = "0.0.0.0"
-UDP_IP2 = "10.1.138.73"
+UDP_IP2 = "10.1.137.32"
 
-UDP_PORT = 65432
+UDP_PORT = 10002
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.bind(("0.0.0.0", UDP_PORT))
+sock.bind((MINE, UDP_PORT))
 
 while True:
 	data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-	var = struct.unpack('iiii?',data)
+	var = struct.unpack('BiiB?',data)
 	t = var[1]
 	t = time.ctime(t)
 	packAck=struct.pack('ii',var[0],var[2]) 
-	sock.sendto(packAck, (UDP_IP2, UDP_PORT))
-	print "received message:", var[0], t, var[2], var[3], var[4]
+	sock.sendto(packAck, (addr))
+	print ("received message:", var[0], t, var[2], var[3], var[4] )
