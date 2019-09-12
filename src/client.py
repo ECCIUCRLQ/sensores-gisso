41b047197308d84e7bce8aca845e8dd0f4f04b47
Ivan Chavarria <ivanjavier2000@gmail.com>
2019-09-12 10:15:22 -0600

    Implementacion del timeout: Si no se recibe ACK en un tiempo de 5 segundos, se reenvia, si despues de 4 intentos no llega nada, se deja de enviar y se identifica como conección perdida. Realizado por Steff e Ivan. close #6

--- src/client.py
+++ src/client.py
@@ -2,8 +2,10 @@ import socket
 import struct
 import time
 import random
+import select
 
-UDP_IP = "10.1.137.192" #IP a enviar
+
+UDP_IP = "10.1.138.31" #IP a enviar
 MINE = "10.1.137.17"   # Mi ip, para el bind
 
 UDP_PORT = 10001
@@ -27,15 +29,39 @@ var = struct.pack(FORMAT,randomId,date,TEAM_ID,0,0,sensorId,code,data)
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((MINE, UDP_PORT))
+check = 0
+contador = 0
 
-sock.sendto(var, (UDP_IP, UDP_PORT))
+while check == 0 and contador < 4:
+	sock.sendto(var, (UDP_IP, UDP_PORT))
 
-data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-ACK = struct.unpack('BBBBB', data)
 
-if randomId == ACK[0] :
-	print ("Mensaje enviado y recibido correctamente")
-else :
-	print ("Mensaje enviado y recibido incorrectamente")
+	ready = select.select([sock], [], [], 5) #timeout de 5 segundos
+	
+	if ready[0]:
+		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+		ACK = struct.unpack('BBBBB', data)
+		
+		if randomId == ACK[0] :
+			print ("------RECIEVED-------\nACK Number Recieved =",ACK[0])
+			print ("From =", ACK[1])
+			print ("Sensor =", ACK[4])
+			print ("--------------------------\n")
+			check = 1
+		else : #Recibio incorrecto
+			print ("-------ERROR------")
+			print ("Expected ACK =", randomId)
+			print ("Recieved ACK =", ACK[0])
+			print ("Re-sending...")
+			print ("-------------\n")
+			contador+=1
+	else : #No recibio
+		print ("-------------")
+		print ("Missing ACK. Re-sending")
+		print ("-------------\n")
+		contador+=1
 
-print ( "received message:", ACK[0], ACK[1] )
+if check == 0 :
+	print ("===============")
+	print ("Conection lost")
+	print ("===============")
