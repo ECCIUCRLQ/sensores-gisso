4c4e9e88a35ca849c526f5753d32c1ee3a14fc43
Ivan Chavarria <ivanjavier2000@gmail.com>
2019-09-12 08:45:28 -0600

    Correccion en errores de formato. Realizado entre todos

--- src/client.py
+++ src/client.py
@@ -3,24 +3,26 @@ import struct
 import time
 import random
 
-UDP_IP = "10.1.137.79"
-MINE = "10.1.137.17"
-UDP_PORT = 10002
+UDP_IP = "10.1.137.192" #IP a enviar
+MINE = "10.1.137.17"   # Mi ip, para el bind
+
+UDP_PORT = 10001
 
 SHOCK_ID = 0x04
 MOV_ID = 0x01
 KEEP_ALIVE = 0x0
 TEAM_ID = 0x03
 BOOL_TYPE = 0x01
+FORMAT = 'BiBBBBBf'
 
 date = int( time.time() )
 randomId= random.randint(0,255)
-sensorId = 000
+sensorId = SHOCK_ID
 code = KEEP_ALIVE
 data = True
 
 
-var = struct.pack('BiiB?',randomId,date,sensorId,code,data) 
+var = struct.pack(FORMAT,randomId,date,TEAM_ID,0,0,sensorId,code,data) 
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
@@ -29,7 +31,7 @@ sock.bind((MINE, UDP_PORT))
 sock.sendto(var, (UDP_IP, UDP_PORT))
 
 data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-ACK = struct.unpack('ii', data)
+ACK = struct.unpack('BBBBB', data)
 
 if randomId == ACK[0] :
 	print ("Mensaje enviado y recibido correctamente")
--- src/server.py
+++ src/server.py
@@ -2,12 +2,10 @@ import socket
 import struct 
 import time
 
-
 MINE = "10.1.137.17"
-UDP_IP = "0.0.0.0"
-UDP_IP2 = "10.1.137.32"
 
-UDP_PORT = 10002
+UDP_PORT = 10001
+FORMAT = 'BiBBBBBf'
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
@@ -15,9 +13,9 @@ sock.bind((MINE, UDP_PORT))
 
 while True:
 	data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-	var = struct.unpack('BiiB?',data)
+	var = struct.unpack(FORMAT,data)
 	t = var[1]
 	t = time.ctime(t)
-	packAck=struct.pack('ii',var[0],var[2]) 
+	packAck=struct.pack('BBBBB',var[0],var[2],0,0, var[5]) 
 	sock.sendto(packAck, (addr))
 	print ("received message:", var[0], t, var[2], var[3], var[4] )
