e1cfd760a748388e78037832eeceaa654f901b42
Ivan Javier Chavarria Vega <b72097@ecci.ucr.ac.cr@ECCI380394L0107ecci.ucr.ac.cr>
2019-09-16 15:33:00 -0600

    El cliente recibe informacion de los sensores, y dependiendo en lo que reciba decide que enviar. Nota: No se han realizado test debido a que carecemos permisos de administrador. Realizdo por Steff e Ivan. Close #8 Close #9

--- src/client.py
+++ src/client.py
@@ -3,64 +3,86 @@ import struct
 import time
 import random
 import select
-
+from ipcqueue import sysvmq
 
 UDP_IP = "10.1.138.31" #IP a enviar
 MINE = "10.1.137.17"   # Mi ip, para el bind
 
-UDP_PORT = 10001
+UDP_PORT = 10032
 
 SHOCK_ID = 0x04
 MOV_ID = 0x01
 KEEP_ALIVE = 0x0
 TEAM_ID = 0x03
 BOOL_TYPE = 0x01
-FORMAT = 'BiBBBBBf'
+FORMAT = 'BIBBBBBf'
 
-date = int( time.time() )
-randomId= random.randint(0,255)
+shock =sysvmq.Queue(1)
+mov = sysvmq.Queue(2)
+
+
 sensorId = SHOCK_ID
-code = KEEP_ALIVE
-data = True
 
-var = struct.pack(FORMAT,randomId,date,TEAM_ID,0,0,sensorId,code,data) 
+lastRandomId = 0
+randomId = -1
+
+
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((MINE, UDP_PORT))
-check = 0
+
 contador = 0
 
-while check == 0 and contador < 4:
-	sock.sendto(var, (UDP_IP, UDP_PORT))
+def enviar(var):
+	check = 0
+	while check == 0 :#and contador < 4:
+		sock.sendto(var, (UDP_IP, UDP_PORT))
 
 
-	ready = select.select([sock], [], [], 5) #timeout de 5 segundos
-	
-	if ready[0]:
-		data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-		ACK = struct.unpack('BBBBB', data)
+		ready = select.select([sock], [], [], 5) #timeout de 5 segundos
 		
-		if randomId == ACK[0] :
-			print ("------RECIEVED-------\nACK Number Recieved =",ACK[0])
-			print ("From =", ACK[1])
-			print ("Sensor =", ACK[4])
-			print ("--------------------------\n")
-			check = 1
-		else : #Recibio incorrecto
-			print ("-------ERROR------")
-			print ("Expected ACK =", randomId)
-			print ("Recieved ACK =", ACK[0])
-			print ("Re-sending...")
-			print ("-------------\n")
-			contador+=1
-	else : #No recibio
-		print ("-------------")
-		print ("Missing ACK. Re-sending")
-		print ("-------------\n")
-		contador+=1
-
-if check == 0 :
-	print ("===============")
-	print ("Conection lost")
-	print ("===============")
+		if ready[0]:
+			data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+			ACK = struct.unpack('BBBBB', data)
+			
+			if randomId == ACK[0] :
+				print ("------RECIEVED-------\nACK Number Recieved =",ACK[0])
+				print ("From =", ACK[1])
+				print ("Sensor =", ACK[4])
+				print ("--------------------------\n")
+				check = 1
+			
+				
+while True: 
+	randomId= random.randint(0,255)
+	while(randomId == lastRandomId):
+		randomId = random.randint(0,255)
+		
+	dateshock = shock.get()
+	if dateshock == 0 :
+		date = int(time.time())
+		packshock = struct.pack(FORMAT,randomId,date,TEAM_ID,0,0,SHOCK_ID,KEEP_ALIVE,0)
+		enviar(packshock)
+	
+	else: 
+		packshock = struct.pack(FORMAT,randomId,dateshock,TEAM_ID,0,0,SHOCK_ID,BOOL_TYPE,1)
+		enviar(packshock)
+		
+	lastRandomId = randomId
+	
+	randomId= random.randint(0,255)
+	while(randomId == lastRandomId):
+		randomId = random.randint(0,255)	
+	
+	datemov = mov.get()
+	if datemov == 0 :
+		date = int(time.time())
+		packmov = struct.pack(FORMAT,randomId,date,TEAM_ID,0,0,MOV_ID,KEEP_ALIVE,0)
+		enviar(packmov)
+	
+	else: 
+		packmov = struct.pack(FORMAT,randomId,datemov,TEAM_ID,0,0,MOV_ID,BOOL_TYPE,1)
+		enviar(packmov)
+		
+		
