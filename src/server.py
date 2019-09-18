a41835e575739c1377514df1f1a1aaebccb9b28c
Ivan Chavarria <ivanjavier2000@gmail.com>
2019-09-18 16:33:08 -0600

    Se guardan los datos recibidos en un archivo de texto. Realizado por Sebastian e Ivan

--- src/server.py
+++ src/server.py
@@ -2,16 +2,18 @@ import socket
 import struct 
 import time
 
-MINE = "10.1.138.139"
+MINE = "10.1.137.9"
 
-UDP_PORT = 10001
-FORMAT = 'BiBBBBBf'
+UDP_PORT = 5000
+FORMAT = 'BIBBBBBf'
 ListaIdGrupo=["Whitenoise","FlamingoBlack","GISSO","KOF","Equipo 404","Poffis"]
 listaSensores=["KeepAlive","Movimiento","Sonido","Luz","Shock","Touch","Humedad","BigSound","Temperatura","Ultrasonico"]
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((MINE, UDP_PORT))
+
+archivo = open("datos.txt","w")
 
 while True:
 	data, addr = sock.recvfrom(50) # buffer size is 1024 bytes
@@ -20,9 +22,10 @@ while True:
 	t = time.ctime(t)
 	packAck=struct.pack('BBBBB',var[0],var[2],0,0, var[5]) 
 	sock.sendto(packAck, (addr))
-	print ("Mensaje recibido de: "+ str(ListaIdGrupo[var[2]-1]) )
-	print ("Sensor: "+ str(listaSensores[var[6]]) )
-	print ("Numero Ack: "+ str(var[0]))
-	print ("Fecha: " + str(t))
-	print ("Dato: " + str(var[7]))
-	print ("----------------------------------------------------------")
+
+	aEscribir = "Mensaje recibido de: " + ListaIdGrupo[var[2]-1] +"\n"+"Sensor: "+listaSensores[var[6]]+"\n"+"Numero Ack: "+str(var[0])+"\n"+"Fecha: " +str(t)+"\n"+"Dato: " +str(var[7])+"\n-------------------\n"
+	
+	print(aEscribir)	
+	archivo.write(aEscribir)
+	
+archivo.close()	
