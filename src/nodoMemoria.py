0af10dd1fb754f4296e73baa0988c0583e1258b9
SteffFernandez <steff540@gmail.com>
2019-11-22 21:27:29 +0000

    Pruebas de comunicacion

--- src/nodoMemoria.py
+++ src/nodoMemoria.py
@@ -1,4 +1,6 @@
 import sys
+import socket
+#from socket import *
 import time
 import struct
 #Metadatos Generales:
@@ -13,6 +15,7 @@ import struct
 #-Fecha Consulta 4 bytes
 punteroMeta = 0
 punteroDatos = 4
+IDIP = 0
 
 if( len(sys.argv) < 2 ):
 	print ("NodoMemoria: Error, argumentos insuficientes")
@@ -116,46 +119,105 @@ def buscarDatos(idPagina):
 	
 	return datosCompletos
 		
+		
+def broadcast():
+	global IDIP
+	
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+	#sock.settimeout(5)
+	sock.setblocking(0)
+
+	server_address = ('255.255.255.255', 5000)
+	message = 'pfg_ip_broadcast_cl'
+
+	try:
+		while True:
+			# Send data
+			#print('Ivanxd sending: ' + message)
+			sent = sock.sendto(message.encode(), server_address)
+
+			# Receive response
+			#print('waiting to receive')
+			try:
+				data, server = sock.recvfrom(4096)
+				if data.decode('UTF-8') == 'pfg_ip_response_serv':
+					print('Received confirmation')
+					IDIP = server[0]
+					print('Server ip: ' + str(server[0]) )
+					break
+				else:
+					print('Verification failed')
+			except:
+				print('Soy nodo deme pelota')
+			time.sleep(2)	
+	finally:	
+		sock.close()
+	
+def recibirTCP():
+	global IDIP
+	  
+	PORT = 3114        # Port to listen on (non-privileged ports are > 1023)
+	print("hola")
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.bind(('0.0.0.0', PORT))
+		print("hola22")
+		s.listen()
+		print("hola23")
+		conn, addr = s.accept()
+		print("hola54")
+		print(adrr)
+		with conn:
+			print('Connected by', addr)
+			while True:
+				data = conn.recv(1024)
+				if not data:
+					break
+				conn.sendall(data)
+		
+	
 def test():
 	
 	crearArchivo()
+	broadcast()
+	recibirTCP()
+	# ~ datos = bytearray([0,2,3,4,5])
+	# ~ datos1 = bytearray([1,2,3,4,5])
+	# ~ datos2 = bytearray([2,2,3,4,5])
+	# ~ formatoGuardar = "BBI" + str(len(datos)) +"s"
+	# ~ formatoGuardar1 = "BBI" + str(len(datos1)) +"s"
+	# ~ formatoGuardar2 = "BBI" + str(len(datos2)) +"s"
+	# ~ pagID = 2
+	# ~ tamPag = len(datos) # * 5 con este 5 lo mandan desde mem local
 	
-	datos = bytearray([0,2,3,4,5])
-	datos1 = bytearray([1,2,3,4,5])
-	datos2 = bytearray([2,2,3,4,5])
-	formatoGuardar = "BBI" + str(len(datos)) +"s"
-	formatoGuardar1 = "BBI" + str(len(datos1)) +"s"
-	formatoGuardar2 = "BBI" + str(len(datos2)) +"s"
-	pagID = 2
-	tamPag = len(datos) # * 5 con este 5 lo mandan desde mem local
+	# ~ print ("Len datos", len(datos))
 	
-	print ("Len datos", len(datos))
+	# ~ packGuardar = struct.pack(formatoGuardar,0,1,tamPag,datos)
+	# ~ packGuardar1 = struct.pack(formatoGuardar1,0,2,tamPag,datos1)
+	# ~ packGuardar2 = struct.pack(formatoGuardar2,0,3,tamPag,datos2)
 	
-	packGuardar = struct.pack(formatoGuardar,0,1,tamPag,datos)
-	packGuardar1 = struct.pack(formatoGuardar1,0,2,tamPag,datos1)
-	packGuardar2 = struct.pack(formatoGuardar2,0,3,tamPag,datos2)
+	# ~ paquete = struct.unpack(formatoGuardar,packGuardar)
+	# ~ paquete1 = struct.unpack(formatoGuardar1,packGuardar1)
+	# ~ paquete2 = struct.unpack(formatoGuardar2,packGuardar2)
 	
-	paquete = struct.unpack(formatoGuardar,packGuardar)
-	paquete1 = struct.unpack(formatoGuardar1,packGuardar1)
-	paquete2 = struct.unpack(formatoGuardar2,packGuardar2)
+	# ~ paqueteQuiero = [paquete[1],paquete[2],paquete[3]]
+	# ~ paqueteQuiero1 = [paquete1[1],paquete1[2],paquete1[3]]
+	# ~ paqueteQuiero2 = [paquete2[1],paquete2[2],paquete2[3]]
 	
-	paqueteQuiero = [paquete[1],paquete[2],paquete[3]]
-	paqueteQuiero1 = [paquete1[1],paquete1[2],paquete1[3]]
-	paqueteQuiero2 = [paquete2[1],paquete2[2],paquete2[3]]
+	# ~ agregarDatos(paqueteQuiero)
+	# ~ agregarDatos(paqueteQuiero1)
+	# ~ agregarDatos(paqueteQuiero2)
 	
-	agregarDatos(paqueteQuiero)
-	agregarDatos(paqueteQuiero1)
-	agregarDatos(paqueteQuiero2)
+	# ~ print ("Paquete Guarde:",datos)
 	
-	print ("Paquete Guarde:",datos)
+	# ~ datosVuelta = buscarDatos(1)
+	# ~ datosVuelta1 = buscarDatos(2)
+	# ~ datosVuelta2 = buscarDatos(3)
 	
-	datosVuelta = buscarDatos(1)
-	datosVuelta1 = buscarDatos(2)
-	datosVuelta2 = buscarDatos(3)
-	
-	print ("Paquete pedi:", datosVuelta)
-	print ("Paquete pedi:", datosVuelta1)
-	print ("Paquete pedi:", datosVuelta2)
+	# ~ print ("Paquete pedi:", datosVuelta)
+	# ~ print ("Paquete pedi:", datosVuelta1)
+	# ~ print ("Paquete pedi:", datosVuelta2)
 
 
 test()
